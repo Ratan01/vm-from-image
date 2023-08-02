eda6013df928004cdf6441fcf76f8f776228806a
Ratan01 <ratanrudra010@gmail.com>
2023-08-02 15:24:14 +0530

    edited maint.tf

--- src/main.tf
+++ src/main.tf
@@ -7,16 +7,16 @@ provider "azurerm" {
 }
 
 # Locate the existing resource group
-data "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "rg" {
   name = "ratan"
 }
 
 output "id" {
-  value = data.azurerm_resource_group.main.id
+  value = data.azurerm_resource_group.rg.id
 }
 
 # Locate the existing custom image
-data "azurerm_image" "main" {
+data "azurerm_image" "img" {
   name                = "ratan_vm_image_dn"
   resource_group_name = "ratan"
 }
@@ -26,10 +26,10 @@ output "image_id" {
 }
 
 # Create a Network Security Group with some rules
-resource "azurerm_network_security_group" "main" {
+resource "azurerm_network_security_group" "sg" {
   name                = "my-SG"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "my-SGR"
@@ -45,26 +45,26 @@ resource "azurerm_network_security_group" "main" {
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "vim" {
   name                = "my-network"
   address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 # Create subnet
-resource "azurerm_subnet" "main" {
+resource "azurerm_subnet" "sub" {
   name                 = "my-subnet"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vim.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Create public IP
-resource "azurerm_public_ip" "main" {
+resource "azurerm_public_ip" "pip" {
   name                = "my-public-ip"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   allocation_method   = "Static"
 
   tags = {
@@ -73,31 +73,31 @@ resource "azurerm_public_ip" "main" {
 }
 
 # Create network interface
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "nic" {
   name                = "my-nic"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
+    subnet_id                     = azurerm_subnet.sub.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
 # Create a new Virtual Machine based on the custom Image
 resource "azurerm_virtual_machine" "myVM2" {
   name                             = "myVM2"
-  location                         = data.azurerm_resource_group.main.location
-  resource_group_name              = data.azurerm_resource_group.main.name
-  network_interface_ids            = ["${azurerm_network_interface.main.id}"]
+  location                         = data.azurerm_resource_group.rg.location
+  resource_group_name              = data.azurerm_resource_group.rg.name
+  network_interface_ids            = ["${azurerm_network_interface.nic.id}"]
   vm_size                          = "Standard_DS12_v2"
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
 
   storage_image_reference {
-    id = "${data.azurerm_image.main.id}"
+    id = "${data.azurerm_image.img.id}"
   }
 
   storage_os_disk {
